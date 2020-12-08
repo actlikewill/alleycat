83ce5be743d01ba48928b14eddc7445433ab8bdd
actlikewill <wilsongaturu@gmail.com>
2020-12-08 23:41:58 +0300

    adds admin switcher with javascript

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,15 @@
-console.log('alleycat is live')
+window.addEventListener( "load",  () => {  
+    const tabs = document.querySelectorAll("ul.nav-tabs > li");
+    const setListener = (tab) => {tab.addEventListener("click", switchTab)};
+    const switchTab = (event) => {
+            event.preventDefault();
+            document.querySelector("ul.nav-tabs li.active").classList.remove("active");   
+            document.querySelector(".tab-pane.active").classList.remove("active");       
+            const clickedTab = event.currentTarget;
+            const anchor = event.target;
+            const activePaneID = anchor.getAttribute("href");
+            clickedTab.classList.add("active");
+            document.querySelector(activePaneID).classList.add("active");
+            };
+    tabs.forEach(setListener);    
+}); 
--- inc/Base/Enqueue.php
+++ inc/Base/Enqueue.php
@@ -7,16 +7,15 @@ namespace Inc\Base;
 
 class Enqueue extends \Inc\Base\BaseController
 {
-
     public function register() 
     {
-        add_action( 'admin_enqueue_scripts', array( $this, 'enqueue') );;  
+        add_action( 'admin_enqueue_scripts', array( $this, 'enqueue') );  
     }
 
     /**
      * Enqueue admin scripts
      */
-    function enqueue() {        
+    function enqueue() {              
         wp_enqueue_style('AlleyCatStyles', $this->plugin_url . 'assets/styles.css' );
         wp_enqueue_script('AlleyCatScripts', $this->plugin_url . 'assets/scripts.js' );
         }
